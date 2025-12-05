91ec69ed6178370701bddbff172548f806474fc1
Aaliyah Harper <aaliyah@aileks.dev>
2025-12-05 10:14:54 -0500

    add text node classes

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
+from textnode import TextNode, TextType
+
+
 def main():
-    print("Hello world!")
+    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(node)
 
 
 if __name__ == "__main__":
